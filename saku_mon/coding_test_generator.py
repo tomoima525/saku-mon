880cc450b4f304e85febfa51d797ed4c2328dd9f
Tomoaki Imai <tomoima525@gmail.com>
2024-12-01 08:20:14 -0800

    support private github repo

--- saku_mon/coding_test_generator.py
+++ saku_mon/coding_test_generator.py
@@ -1,6 +1,6 @@
 import os
-from git import Repo
 import shutil
+from github import Github, Auth
 from saku_mon.llm_client import OpenAIClient
 from saku_mon.output_structures import CodingTestIdea
 
@@ -17,27 +17,51 @@ extensions_to_extract = {
 }
 
 class CodingTestGenerator:
-    def __init__(self, repo_url):
-        self.repo_url = repo_url
+    def __init__(self, repo_path):
+        self.repo_path = repo_path
+        self.repo_owner = repo_path.split("/")[0]
         self.project_root = os.path.abspath(os.path.join(os.getcwd()))
         self.output_file = f"{self.project_root}/output/repo_contents.txt"
         os.makedirs(os.path.dirname(self.output_file), exist_ok=True)
         self.llm_client = OpenAIClient()
+        auth = Auth.Token(os.getenv('GITHUB_API_KEY'))
+        self.github_client = Github(auth=auth)
+        print(f"GitHub user: {self.github_client.get_user().login}")
 
-    def clone_repo(self):
-        repo_name = self.repo_url.split("/")[-1].replace(".git", "")
+    def clone_repo(self, force_clone=False):
+        repo_name = self.repo_path.split("/")[-1].replace(".git", "")
         self.clone_dir = f"{self.project_root}/repo/{repo_name}"
+        if force_clone:
+            shutil.rmtree(self.clone_dir, ignore_errors=True)
         if os.path.exists(self.clone_dir):
-            # shutil.rmtree(self.clone_dir)
             print(f"Repository already cloned to {self.clone_dir}")
             return
-        Repo.clone_from(self.repo_url, self.clone_dir)
+        # Handle private repository authentication using PyGithub
+        api_key = os.getenv('GITHUB_API_KEY')
+        if api_key:
+            repo = self.github_client.get_repo(f"{self.repo_path}")
+            archive_url = repo.get_archive_link('zipball')
+            auth_repo_url = archive_url.replace("https://", f"https://oauth2:{api_key}@")
+        else:
+            auth_repo_url = self.repo_url
+        # Download and extract the repository
+        print(f"Cloning repository ...")
+        os.system(f"curl -L {auth_repo_url} -o {self.clone_dir}.zip")
+        shutil.unpack_archive(f"{self.clone_dir}.zip", f"{self.project_root}/repo")
+        os.remove(f"{self.clone_dir}.zip")
+        # Move extracted files to clone_dir
+        extracted_dir = next(os.path.join(f"{self.project_root}/repo", d) for d in os.listdir(f"{self.project_root}/repo") if d.startswith(self.repo_owner))
+        shutil.move(extracted_dir, self.clone_dir)
         print(f"Repository cloned to {self.clone_dir}")
     
     def get_project_type(self):
         """
         Check the cloned repository and return the file extensions to extract.
         """
+        print(f"Checking project type for {self.clone_dir}")
+        # Check if pyproject.toml exists
+        exists =os.path.exists(f"{self.clone_dir}/pyproject.toml")
+        print(f" as {exists}")
         if os.path.exists(f"{self.clone_dir}/package.json"):
             return ProjectType.TYPESCRIPT
         # if requirements.txt or pyproject.toml or setup.py exists, it is a python project 
@@ -125,8 +149,8 @@ class CodingTestGenerator:
             f.write(solution_code)
         print("Example solution generated!")
 
-    def run(self):
-        self.clone_repo()
+    def run(self, force_clone=False):
+        self.clone_repo(force_clone=force_clone)
         project_type = self.get_project_type()
         print(f"Project type: {project_type.value if project_type else 'Unknown'}")
         file_extensions = extensions_to_extract[project_type.value] if project_type else None
